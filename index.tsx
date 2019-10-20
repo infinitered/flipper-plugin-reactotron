394ba9a70efe40e2205dd398bed0781b46e073bc
rmevans9 <rmevans9@gmail.com>
2019-10-20 17:28:25 -0400

    feat: Header and Footer
    * Adding header and footer
    
    * Update to using the header in core-ui

--- index.tsx
+++ index.tsx
@@ -1,11 +1,51 @@
 import { FlipperPlugin } from "flipper";
-import { theme, timelineCommandResolver } from "reactotron-core-ui";
-import { ThemeProvider } from "styled-components";
+import { theme, timelineCommandResolver, Header } from "reactotron-core-ui";
+import styled, { ThemeProvider } from "styled-components";
 
-export default class extends FlipperPlugin<never, never, { commands: any[] }> {
+import logo from "./logo";
+import logo2 from "./logo2";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  background-color: ${props => props.theme.background};
+`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  background-color: ${props => props.theme.backgroundSubtleLight};
+  border-bottom: 1px solid ${props => props.theme.chromeLine};
+  color: ${props => props.theme.foregroundDark};
+  box-shadow: 0 0 30px ${props => props.theme.glow};
+  height: 70px;
+`;
+
+const TimelineContainer = styled.div`
+  flex: 1;
+  overflow-y: scroll;
+`;
+
+const FooterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  background-color: ${props => props.theme.backgroundSubtleLight};
+  border-top: 1px solid ${props => props.theme.chromeLine};
+  color: ${props => props.theme.foregroundDark};
+  height: 35px;
+  padding: 10px;
+`;
+
+interface PersistedState {
+  commands: any[];
+}
+
+export default class extends FlipperPlugin<never, never, PersistedState> {
   static defaultPersistedState = { commands: [] };
 
-  static persistedStateReducer<PersistedState>(
+  static persistedStateReducer(
     persistedState: PersistedState,
     method: string,
     data: Object
@@ -18,19 +58,23 @@ export default class extends FlipperPlugin<never, never, { commands: any[] }> {
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <div>
-          {this.props.persistedState.commands.map(command => {
-            const CommandComponent = timelineCommandResolver(command.type);
+        <Container>
+          <Header title="Timeline" />
+          <TimelineContainer>
+            {this.props.persistedState.commands.map(command => {
+              const CommandComponent = timelineCommandResolver(command.type);
 
-            console.log(command.date);
+              if (CommandComponent) {
+                return <CommandComponent command={command} />;
+              }
 
-            if (CommandComponent) {
-              return <CommandComponent command={command} />;
-            }
-
-            return null;
-          })}
-        </div>
+              return null;
+            })}
+          </TimelineContainer>
+          <FooterContainer>
+            <img src={`data:image/png;base64, ${logo2}`} height={30} />
+          </FooterContainer>
+        </Container>
       </ThemeProvider>
     );
   }
