d9a8eb9c2438c62476a84d9e6dae24ca0af59132
Leon Kim <minusnine@gmail.com>
2019-12-03 04:57:29 +0900

    add: Update subscription state by changes

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,53 @@ interface PersistedState {
   onTab: "timeline" | "subscriptions"
 }
 
+const compareChanges = (newChange, oldChange) => {
+  for (let key in oldChange) {
+    if (!(key in newChange)) {
+      return { removed: oldChange }
+    }
+  }
+  for (let key in newChange) {
+    if (!(key in oldChange)) {
+      return { added: newChange }
+    }
+
+    const newChangeValues = JSON.stringify(newChange[key]) || ""
+    const oldChangeValues = JSON.stringify(oldChange[key]) || ""
+
+    if ((newChangeValues.length - oldChangeValues.length != 0)
+      || newChangeValues !== oldChangeValues) {
+      return { changed: newChange }
+    }
+  }
+  return null
+}
+
+const rewriteChangesSinceLastStateSubscription = (state, data) => {
+  if (data.type != "state.values.change") {
+    return
+  }
+
+  const objectFromSubscriptionPair = (pair: {}) => {
+    let obj = {}
+    obj[pair["path"]] = pair["value"]
+    return obj
+  }
+
+  let oldChange = []
+  const newChange = data["payload"]["changes"].map(change => objectFromSubscriptionPair(change))
+  const changeCommands = state.commands.filter(command => command.type === "state.values.change")
+
+  if (changeCommands.length) {
+    const latestChange = changeCommands[0]
+    oldChange = latestChange["payload"]["changes"].map(change => objectFromSubscriptionPair(change))
+  }
+
+  const diff = compareChanges(newChange, oldChange)
+
+  data["payload"] = { ...data["payload"], ...diff }
+}
+
 export default class extends FlipperPlugin<never, never, PersistedState> {
   static defaultPersistedState = {
     commands: [],
@@ -20,6 +67,9 @@ export default class extends FlipperPlugin<never, never, PersistedState> {
     method: string,
     data: Record<string, any>
   ): PersistedState {
+
+    rewriteChangesSinceLastStateSubscription(persistedState, data)
+
     return {
       ...persistedState,
       commands: [
